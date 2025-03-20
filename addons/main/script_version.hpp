84b824ccf55f1728ec86000beb63813615433da5
Mike-MF <TyroneMF@hotmail.com>
2025-03-20 17:23:02 +0000

    Bump version to 1.1.0

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,10 +1,10 @@
 #define MAJOR 1
-#define MINOR 0
+#define MINOR 1
 #define PATCHLVL 0
 #define BUILD 0
 
 /*
  * Custom version to start at 1.0.0
  * IEDD original starts at 1.8.1
- * Version disables utilisation of VBIeds.
+ * Version disables utilisation of VBIeds. & Suicide Bombers
 */
